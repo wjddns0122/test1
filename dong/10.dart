da17b84b528b04fbfb50887903a30cb1109d1c9d
wjddns0122 <64579991+wjddns0122@users.noreply.github.com>
2023-05-09 17:35:05 +0900

    ing

--- dong/10.dart
+++ dong/10.dart
@@ -1,25 +1,25 @@
-// void main() {
-//   Cal calculate = Cal(10, 20);
-//   print(calculate.firstNumber);
-//   print(calculate.secondNumber);
+void main() {
+  Cal calculate = Cal(10, 20);
+  print(calculate.firstNumber);
+  print(calculate.secondNumber);
 
-//   print(calculate.firstNumber + calculate.secondNumber);
-//   print(calculate.firstNumber - calculate.secondNumber);
-//   print(calculate.firstNumber * calculate.secondNumber);
-//   print(calculate.firstNumber / calculate.secondNumber);
-// }
+  print(calculate.firstNumber + calculate.secondNumber);
+  print(calculate.firstNumber - calculate.secondNumber);
+  print(calculate.firstNumber * calculate.secondNumber);
+  print(calculate.firstNumber / calculate.secondNumber);
+}
 
-// class Cal {
-//   int a;
-//   int b;
+class Cal {
+  int a;
+  int b;
 
-//   Cal(this.a, this.b);
+  Cal(this.a, this.b);
 
-//   int get firstNumber {
-//     return this.a;
-//   }
+  int get firstNumber {
+    return this.a;
+  }
 
-//   int get secondNumber {
-//     return this.b;
-//   }
-// }
+  int get secondNumber {
+    return this.b;
+  }
+}
